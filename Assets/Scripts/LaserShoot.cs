307e3c6b3e7cef6c45cd54612fe7fb2992b43f0d
Alpha2402 <84977347+Alpha2402@users.noreply.github.com>
2021-06-04 17:04:59 +0200

    Update : add duration life for the laser
    delete laser

--- Assets/Scripts/LaserShoot.cs
+++ Assets/Scripts/LaserShoot.cs
@@ -6,6 +6,7 @@ public class LaserShoot : MonoBehaviour
 {
     public GameObject Projectil;
     public int force = 10;
+    public float durationLaser = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,8 @@ public class LaserShoot : MonoBehaviour
             //GameObject Laser = Instantiate(Projectil, transform.position, Quaternion.identity);
             GameObject Laser = Instantiate(Projectil, transform.position, transform.rotation);
             Laser.GetComponent<Rigidbody>().velocity = Vector3.forward * force;
+
+            Destroy(Laser, durationLaser);
         }
     }
 }
